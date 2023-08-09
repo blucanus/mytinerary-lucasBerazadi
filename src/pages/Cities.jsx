1325e825dac661541eede59a142705eb97e91a98
Lucas <blucanus@gmail.com>
2023-08-09 10:41:10 -0300

    agregado

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -7,7 +7,13 @@ function Cities() {
   let {id} = useParams()
 
   return (
-    <div>{/* {id} */} Cities</div>
+    <>
+      <div>{/* {id} */} Cities</div>
+      <div className="card mt-5">
+        <h2 className="p-5">Hola Probando</h2>
+      </div>
+    </>
+    
   )
 }
 export default Cities
